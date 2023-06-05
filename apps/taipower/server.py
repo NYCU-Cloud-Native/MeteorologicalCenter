49e8e895ee5a50a9a10baac5d4734a83536e61f1
sirius524 <captainhung096@gmail.com>
2023-06-06 03:48:11 +0800

    feat: finish init db

--- apps/taipower/server.py
+++ apps/taipower/server.py
@@ -1,29 +1,49 @@
-from concurrent import futures
-import logging
-
 import grpc
-from crawler import TaipowerCrawler
+from concurrent import futures
+import power_data_pb2
+import power_data_pb2_grpc
+import pandas as pd
+from influxdb import InfluxDBClient
 
+class PowerDataServicer(power_data_pb2_grpc.PowerDataServiceServicer):
+    def GetPowerDataInfo(self, request, context):
+        csv_url = 'https://data.taipower.com.tw/opendata/apply/file/d006019/001.csv'
+
+        # Read the CSV file
+        df = pd.read_csv(csv_url)
+
+        # Prepare the response
+        response = power_data_pb2.PowerDataResponse()
+        response.csvUrl = csv_url
+
+        # Insert data to InfluxDB
+        influx_host = 'localhost'  # Update with your InfluxDB host
+        influx_port = 8086  # Update with your InfluxDB port
+        influx_database = 'your_database'  # Update with your InfluxDB database name
+        influx_measurement = 'power_data'  # Update with your desired measurement name
+
+        client = InfluxDBClient(host=influx_host, port=influx_port)
+        client.create_database(influx_database)
+        client.switch_database(influx_database)
+
+        lines = []
+        for _, row in df.iterrows():
+            for column in df.columns:
+                lines.append(f'{column} value={row[column]}')
+
+        influx_data = [{'measurement': influx_measurement, 'fields': {'value': line}} for line in lines]
+        client.write_points(influx_data)
+
+        client.close()
+
+        return response
 
 def serve():
-    port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-
-    rpc_method_handlers = {
-        'get_data': grpc.unary_unary_rpc_method_handler(
-            TaipowerCrawler.get_data
-        )
-    }
-    handler = grpc.method_handlers_generic_hand
-    ler('crawler.TaipowerCrawler', rpc_method_handlers)
-
-    server.add_generic_rpc_handlers((handler,))
-    server.add_insecure_port('[::]:' + port)
+    power_data_pb2_grpc.add_PowerDataServiceServicer_to_server(PowerDataServicer(), server)
+    server.add_insecure_port('[::]:50051')
     server.start()
-    print("Server started, listening on " + port)
     server.wait_for_termination()
 
-
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
